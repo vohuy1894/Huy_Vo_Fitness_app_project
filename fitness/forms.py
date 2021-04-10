b1a2be4d290514cba5414ad485427fd39011e1ee
Huy Vo <HuyVo@my.unt.edu>
2021-04-10 13:25:23 -0500

    fix_hard coding and css button in signup form

--- fitness/forms.py
+++ fitness/forms.py
@@ -3,12 +3,15 @@ from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from fitness.database import User, Post
 
-
+min_name = 2
+max_name = 20
+min_pw = 10
+max_pw = 100
 class SignUpForm(FlaskForm):
-    first_name = StringField('First name', validators=[DataRequired(), Length(min=2, max=20)])
-    last_name = StringField('Last name', validators=[DataRequired(), Length(min=2, max=20)])
+    first_name = StringField('First name', validators=[DataRequired(), Length(min=min_name, max=max_name)])
+    last_name = StringField('Last name', validators=[DataRequired(), Length(min=min_name, max=max_name)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=min_pw, max=max_pw)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
