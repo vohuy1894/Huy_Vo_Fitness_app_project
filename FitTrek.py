41389e8070cd19de27abd82cfb958e2d1d09226a
Huy Vo <HuyVo@my.unt.edu>
2021-04-10 13:04:56 -0500

    add project

---
+++ FitTrek.py
@@ -0,0 +1,6 @@
+from fit_trek import app
+
+
+##315c4ba357e78911631905f6532c70eac51fb06a
+if __name__ == "__main__":
+    app.run(debug=True)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Huy_Vo_Fitness_app_project
-Create a fitness web app to track user's activities
